c245858942afd7e5c03238115acfb1db58fbad7b
Christoph Thomas <christophthomas@MBPvonChristoph.fritz.box>
2024-01-09 10:27:32 +0100

    Update header

--- components/header.tsx
+++ components/header.tsx
@@ -14,13 +14,13 @@ export default function Header() {
   return (
     <header className="z-[999] relative">
       <motion.div
-        className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
+        className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full dark:bg-opacity-75"
         initial={{ y: -100, x: "-50%", opacity: 0 }}
         animate={{ y: 0, x: "-50%", opacity: 1 }}
       ></motion.div>
 
       <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
-        <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
+        <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium dark:text-gray-400 text-gray-600 sm:w-[initial] sm:flex-nowrap sm:gap-5">
           {links.map((link) => (
             <motion.li
               className="h-3/4 flex items-center justify-center relative"
@@ -30,9 +30,9 @@ export default function Header() {
             >
               <Link
                 className={clsx(
-                  "flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300",
+                  "flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:hover:text-gray-200",
                   {
-                    "text-gray-950 dark:text-gray-200":
+                    "text-gray-950  dark:text-gray-200":
                       activeSection === link.name,
                   }
                 )}
@@ -42,19 +42,26 @@ export default function Header() {
                   setTimeOfLastClick(Date.now());
                 }}
               >
-                {link.name}
-
-                {link.name === activeSection && (
-                  <motion.span
-                    className="bg-gray-100 rounded-full absolute inset-0 -z-10 dark:bg-gray-800"
-                    layoutId="activeSection"
+                {(activeSection === link.name && (
+                  <motion.div
+                    className="relative w-full parent"
+                    initial={false}
+                    layout
+                    // animate={{ opacity: 1, scaleX: 1 }} // Adding scaleX for horizontal scaling animation
+                    animate={{ opacity: 1, scaleX: 1 }}
                     transition={{
-                      type: "spring",
-                      stiffness: 380,
-                      damping: 30,
+                      opacity: { ease: "linear" },
+                      layout: { duration: 0.3 },
                     }}
-                  ></motion.span>
-                )}
+                  >
+                    {link.name}
+                    <motion.div
+                      className="absolute bottom-[-4px] left-1/2 w-[1.4rem] h-[2px] bg-gray-950 dark:bg-gray-200"
+                      style={{ transform: "translateX(-50%)" }} // Centering the underline
+                    ></motion.div>
+                  </motion.div>
+                )) ||
+                  link.name}
               </Link>
             </motion.li>
           ))}
